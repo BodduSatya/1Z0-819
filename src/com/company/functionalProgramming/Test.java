038b94bbaf6d181f59fb91560d3bac53fbafc8b3
Satya <bvvsnc3@gmail.com>
2021-05-02 10:35:52 +0530

    intial commit

--- src/com/company/functionalProgramming/Test.java
+++ src/com/company/functionalProgramming/Test.java
@@ -30,5 +30,33 @@ public class Test {
         Function<List<String>,String> ex4 = x-> x.get(0); // does not compile
         UnaryOperator<Long> ex5 = (Long l2) -> 3L; // does not compile
         Predicate<String> ex6 = String::isEmpty; // does not compile
+
+
+        DoubleFunction<Double> area = r -> Math.PI*r*r;
+        BiFunction<Integer,Integer,Integer> divide = (x,y) -> x/y;
+//        DoublePredicate<Integer,Integer> test = (x,y) -> x == y;
+        IntPredicate test = x -> x == 10;
+
+        /*Function<Double,Double> lambda = r->{
+            double circumference = 2*Math.PI*r;
+        };*/ // missing return statement
+
+        /*String city = "Munich";
+        BiFunction<Integer,Integer,String> lambda = String::substring;*/
+        // String::substring is wrong, city::substring is right
+
+//        BiConsumer<String,String> lambda = (String x,y)-> System.out.println(x+y);
+
+        Consumer<Integer> lambda = (x)-> {
+            x++;
+            System.out.println("x = " + x);
+        };
+
+        List<String> cities;
+        cities = List.of("Berlin","Hamburg","Munich","Cologne","Frankfurt");
+        cities.stream()
+                .filter(city->city.contains("e"))
+                .filter(city->city.contains("l") || city.contains("a"))
+                .forEach(System.out::println);
     }
 }
